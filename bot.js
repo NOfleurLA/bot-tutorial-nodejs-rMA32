123070c163ee92299061d4c33e46846c0a242939
NOfleurLA <31415265+NOfleurLA@users.noreply.github.com>
2018-09-20 22:21:40 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ function respond() {
       botRegexSiege = /^\/siege/; botRegexOW = /^\/ratings/; 
       botRegexDO = /^\/draft order/; botRegexStop = /^\/stop/;
       botRegexPDR = /^\/predraft/;botRegexDB = /^\/draft board/; botRegexSlam = /^\/slam/; botRegexGLD = /^\/glados/;
-      botRegexRP = /^\/roster/; botRegexNFLS = /^\/NFL sdl/; botRegexDD = /^\/dd/;
+      botRegexRP = /^\/roster/; botRegexNFLS = /^\/NFL sdl/; botRegexDD = /^\/dd/; botRegexMM = /^\/Money/;
       botRegexCom = /^\/commands/; botRegexBC = /^\/buddy/; botRegexBRZ = /^\/Breezus/; botRegexBM = /^\/Baker/; botRegexBMD = /^\/Mayfield/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -165,6 +165,11 @@ function respond() {
    else if(request.text && botRegexBMD.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://media.giphy.com/media/RveDcLXAWGizK/giphy.gif");
+    this.res.end();
+  }
+  else if(request.text && botRegexMM.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://media.giphy.com/media/j29c7DGtRTJdK/giphy.gif");
     this.res.end();
   }
   else if(request.text && botRegexSiege.test(request.text)) {
